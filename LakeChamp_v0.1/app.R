c4089fe21c7f05cf02437728b6e0343568bb3ea8
alexnaccarato <ajnaccarato@gmail.com>
2019-01-28 19:50:41 -0500

    Just more comments for myself from the tutorial

--- LakeChamp_v0.1/app.R
+++ LakeChamp_v0.1/app.R
@@ -7,11 +7,30 @@
 #    http://shiny.rstudio.com/
 #
 # Some notes for Alex:
+#
+# Recap from 16:34 
+# 1. Add elements as arguments to fluidPage().
+# 2. Create reactive inputs with an *Input() function.
+# 3. Display reactive results with an *Output() function.
+# 4. Use the serve function to assemble inputs into outputs. 
+# 
 # Rules for using the server function to assemble inputs into outputs...
 # 1. Save the output that you build to output$ -> output$hist for example
 # 2. Build the output with a render*() function -> example is already in the code
-# 3. Access input calues with input$ -> input$num for example
-
+# 3. Access input values with input$ -> input$num for example
+# Create reactivity by using in INPUTS to build RENDERED OUTPUTS
+#
+# Reactive values notes:
+# 1. Reactive values act as the data streams that flow through your app. 
+# 2. The input list is a list of reactive values. The values shoe the current state of the inputs. 
+# 3. You can only call a reactive value from a function that is designed to work with one
+# 4. Reactive values notify. The objects created by reactive functions respond.
+#
+# Reactive functions:
+# 1. Use a code chunk to build (and rebuild) an object...What code will the function use?
+# 2. The object will respind to changes in a set of reactive values...Which reactive values
+#    will the object respond to?
+# You can build many commands into the braces in the render function.
 library(shiny)
 #examplefile
 
