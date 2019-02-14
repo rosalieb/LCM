5e59a3ce9f1282b6d39df52b3c848a6a6fa850af
rosalieb <Rosalie@MacBook-Pro-de-Rosalie.local>
2019-02-14 17:31:04 -0500

    Working shiny app, with 2 tabs - one for plots, one for table

--- R/Shiny_Test_Rosalie.R
+++ R/Shiny_Test_Rosalie.R
@@ -1,18 +1,7 @@
 library(shiny)
 library(ggplot2)
-
-total_app <- total_year[,1:10]
-names(total_app) <- paste("Var",1:10, sep="")
-names(total_app)
-ncol(total_year)
-
-
-###############################################################
-#devtools::install_github("rstudio/shiny")
-#install.packages("devtools")
-#install.packages("DT")
+library(gridExtra)
 library(shiny)
-library(devtools)
 library(DT)
 library(yaml)
 
@@ -20,13 +9,12 @@ out <- total_year
 out$year <- as.numeric(substring(out[,1],1,4))
 str(out$VisitDate)
 out <- out[order(out$year,decreasing = F),]
-myout <- out
 
 # Define UI for slider demo app ----
 ui <- fluidPage(
   
   # App title ----
-  titlePanel("Sliders"),
+  titlePanel("Selection"),
   
   # Sidebar layout with input and output definitions ----
   sidebarLayout(
@@ -34,76 +22,48 @@ ui <- fluidPage(
     # Sidebar to demonstrate various slider options ----
     sidebarPanel(
       # Input: Specification of range within an interval ----
-      sliderInput("range", "Range:",
+      sliderInput("range", "Years selected",
                   min = min(out$year,na.rm=FALSE), max = max(out$year,na.rm=FALSE),
-                  value = c(2000,2001),sep = "")
+                  value = c(2000,2012),sep = ""),
+      conditionalPanel(
+                 'input$id == "graph"',
+                 checkboxGroupInput("toshow", "Columns to show:",
+                 colnames(out)))#, selected = colnames(out)))
+
     ),
     mainPanel(
-      DT::dataTableOutput("mytable")
+      tabsetPanel(
+        id = 'what do you want to see?',
+        tabPanel("graph", plotOutput("myplot1")),
+        tabPanel("table", DT::dataTableOutput("mytable1"))
+      )
     )
   )
+  
 )
-
-# server <- function(input, output) {
-#   
-#   # sorted columns are colored now because CSS are attached to them
-#   # output$mytable <- DT::renderDataTable({
-#   #   DT::datatable(out, options = list(orderClasses = TRUE))
-#   # })
-#   minRowVal <- reactive({
-#     min(which(grepl(input$range[[1]], out$year)))        #Retrieve row number that matches selected range on sliderInput
-#   })
-#   
-#   maxRowVal <- reactive({
-#     max(which(grepl(input$range[[2]], out$year)))        #Retrieve row number that matches selected range on sliderInput
-#   })
-#   
-#   # observeEvent(input$range, {
-#   #   output$mytable <- DT::renderDataTable({
-#   #     DT::datatable[minRowVal():maxRowVal(), ]
-#   #   })
-#   # })
-#   
-#   output$mytable <- DT::renderDataTable({
-#     DT::datatable(out, filter = 'top', options = list(orderClasses = TRUE))
-#   })
-#   
-# }
 
 server <- function(input, output) {
   
-  # data <- makeReactiveBinding("out")
-  # 
-  # # sorted columns are colored now because CSS are attached to them
-  # # output$mytable <- DT::renderDataTable({
-  # #   DT::datatable(out, options = list(orderClasses = TRUE))
-  # # })
-  # minRowVal <- reactive({
-  #   min(which(grepl(as.numeric(input$range[1]), as.numeric(myout$year))), na.rm=T)        #Retrieve row number that matches selected range on sliderInput
-  # })
-  # 
-  # maxRowVal <- reactive({
-  #   max(which(grepl(as.numeric(input$range[2]), as.numeric(myout$year))), na.rm=T)        #Retrieve row number that matches selected range on sliderInput
-  # })
-  # 
-  # 
-  # output$mytable <- DT::renderDataTable({
-  #   DT::datatable(data()[minRowVal():maxRowVal(), ], filter = 'top', options = list(orderClasses = TRUE))
-  # })
-
-  # output$mytable <- DT::renderDataTable({
-  #   DT::datatable(data()[data()$year > input$range[1] && data()$year < input$range[2], ], filter = 'top', options = list(orderClasses = TRUE))
-  # })
+  # plot
+  output$myplot1 <- renderPlot({
+    if (length(input$toshow) == 0) {
+      ggplot(data.frame())
+    } else {
+      gl <- lapply(input$toshow, 
+                   function(b) ggplot(out, aes(x=out[,1],y=out[, b])) +
+                     geom_point() +
+                     xlab("Year") + ylab(b))
+      grid.arrange(grobs = gl, nrow = 1)
+    }
+    
+  })
   
-  # output$mytable <- DT::renderDataTable({
-  #   DT::datatable(data()[data()$year >= as.numeric(input$range[1]) & data()$year <= as.numeric(input$range[2]), ], filter = 'top', options = list(orderClasses = TRUE))
-  # })
-  
-  output$mytable <- DT::renderDataTable({
+  # table
+  output$mytable1 <- DT::renderDataTable({
     DT::datatable(out[out$year >= as.numeric(input$range[1]) & out$year <= as.numeric(input$range[2]), ], filter = 'top', options = list(orderClasses = TRUE))
   })
+  
 }
 
 shinyApp(ui, server)
 
-any(duplicated(colnames(out) )==TRUE)
