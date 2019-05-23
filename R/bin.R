cd5b9c46e3696ae3d108f9d4117a16276dded2ba
Rosalie Bruel <Rosalie.Bruel@uvm.edu>
2019-05-23 14:30:52 -0400

    edit on the temp file

--- R/bin.R
+++ R/bin.R
@@ -1,4 +1,4 @@
-my_sizeclass <- c(seq(50,400,50),seq(500,700,100)) # a vector you may edit to create more size class
+my_sizeclass <- c(seq(50,400,50),seq(500,600,100)) # a vector you may edit to create more size class
 for (i in seq_along(my_sizeclass)) {
   if (i==1) { # everything smaller than the first value of the my_sizeclass vector
     LTdiet_summary <- colSums(LTdiet[which(LTdiet$Total.Length<=my_sizeclass[i]),21:38], na.rm=T)
