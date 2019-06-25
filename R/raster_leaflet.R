e51d4c29ac130d5a18701697ecf060e9aa294f59
alexnaccarato <ajnaccarato@gmail.com>
2019-06-25 12:07:52 -0400

    Full script! For some reason I had two files with the same name, so now this one is updated.

--- R/raster_leaflet.R
+++ R/raster_leaflet.R
@@ -1,10 +1,29 @@
 library(leaflet)
 library(raster)
+library(rgdal)
+library(rnaturalearth)
 
-bathy2.raster.aggregate <- aggregate(bathy2, fact=8)
+bathy<-readOGR(paste0(getpath4data(),"GIS/LakeChamplain_Shapefile/LakeChamplain.shp"))
+lcgroupmap=c("lake",rep("island", 84562))
+bathy2 <- raster(paste0(getpath4data(),"GIS/LCbathy.tif"))
 
-pal <- colorNumeric(c("#0C2C84", "#41B6C4", "#FFFFCC"), values(bathy2.raster.aggregate),
+world <- ne_countries(scale = "medium", returnclass = "sf")
+bathy <- spTransform(bathy,crs(world))
+bathy2 <- projectRaster(bathy2, crs = crs(world))
+
+bathy2.raster.aggregate <- aggregate(bathy2, fact=5)
+
+# #000b70 = blue
+# #159112 = green
+# #fced4b = yellow
+# #f23c24 = red
+
+pal <- colorNumeric(c("#000b70", "#67c7f7", "#159112", "#fced4b", "#f23c24"), values(bathy2.raster.aggregate),
                     na.color = "transparent")
 
 leaflet() %>% addTiles() %>% 
   addRasterImage(bathy2.raster.aggregate, colors = pal, opacity = 0.8)
+
+raster.LC.leaflet <- projectRasterForLeaflet(bathy2.raster.aggregate, method = "ngb")
+
+writeRaster(raster.LC.leaflet, filename = "raster.LC.leaflet", format = "GTiff", overwrite = TRUE)
