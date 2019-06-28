a2dd7415ca21c59b4cc7660724b4b03d53e86175
alexnaccarato <ajnaccarato@gmail.com>
2019-06-28 14:02:57 -0400

    changing dots to underscores

--- R/raster_leaflet.R
+++ R/raster_leaflet.R
@@ -11,20 +11,20 @@ world <- ne_countries(scale = "medium", returnclass = "sf")
 bathy <- spTransform(bathy,crs(world))
 bathy2 <- projectRaster(bathy2, crs = crs(world))
 
-bathy2.raster.aggregate <- aggregate(bathy2, fact=5)
+bathy2_raster_aggregate <- aggregate(bathy2, fact=5)
 
 # #000b70 = blue
 # #159112 = green
 # #fced4b = yellow
 # #f23c24 = red
 
-pal <- colorNumeric(c("#000b70", "#67c7f7", "#159112", "#fced4b", "#f23c24"), values(bathy2.raster.aggregate),
+pal <- colorNumeric(c("#000b70", "#67c7f7", "#159112", "#fced4b", "#f23c24"), values(bathy2_raster_aggregate),
                     na.color = "transparent")
 
 leaflet() %>% addTiles() %>% 
-  addRasterImage(bathy2.raster.aggregate, colors = pal, opacity = 0.8)
+  addRasterImage(bathy2_raster_aggregate, colors = pal, opacity = 0.8)
 
-raster.LC.leaflet <- projectRasterForLeaflet(bathy2.raster.aggregate, method = "ngb")
+raster_LC_leaflet <- projectRasterForLeaflet(bathy2_raster_aggregate, method = "ngb")
 
-writeRaster(raster.LC.leaflet, filename = "LakeChamp_v0.3/data/raster_LC_leaflet", format = "GTiff", overwrite = TRUE)
-writeRaster(bathy2.raster.aggregate, filename = "LakeChamp_v0.3/data/raster_LC", format = "GTiff", overwrite = TRUE)
+writeRaster(raster_LC_leaflet, filename = "LakeChamp_v0.3/data/raster_LC_leaflet", format = "GTiff", overwrite = TRUE)
+writeRaster(bathy2_raster_aggregate, filename = "LakeChamp_v0.3/data/raster_LC", format = "GTiff", overwrite = TRUE)
